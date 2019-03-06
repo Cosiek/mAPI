797bc9927bbfc8eb36ae5ae5fe4529077c8993d3
cosiek <witold.majchrzak@21s.pl>
2019-03-06 23:14:43 +0100

    Add Movie model

--- mAPI/movies/models.py
+++ mAPI/movies/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.PositiveSmallIntegerField()
+    runtime = models.PositiveSmallIntegerField(verbose_name="Runtime in minutes")
+    genre = models.CharField(max_length=65)
+    director = models.CharField(max_length=65)
+    country = models.CharField(max_length=65)
