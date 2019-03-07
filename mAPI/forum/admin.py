42f38ac1b8f07ddb418ff945dfdeabc043821ed8
cosiek <witold.majchrzak@21s.pl>
2019-03-07 23:57:47 +0100

    Register comment model at django admin

--- mAPI/forum/admin.py
+++ mAPI/forum/admin.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment
+
+admin.site.register(Comment, admin.ModelAdmin)
