d0ade85e1ad86455a2ba1d904bfc959cc1bc402c
cosiek <witold.majchrzak@21s.pl>
2019-03-07 23:56:11 +0100

    Add Comments model

--- mAPI/forum/models.py
+++ mAPI/forum/models.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    movie = models.ForeignKey('movies.Movie', related_name='comments',
+                              on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '{movie_id}: {txt}'.format(
+            movie_id=self.movie_id,
+            txt=self.text[:23] + ('…' if self.text[23:] else ''))
